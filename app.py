ccfe7d474aa4a04e508f7a754c88adde21006ed2
Alberto Romero <optimussky05@gmail.com>
2023-08-23 19:42:56 -0600

    First test using SANIC python framework

---
+++ app.py
@@ -0,0 +1,48 @@
+from sanic import Sanic
+from sanic.response import json
+from sanic import response
+from jinja2 import Environment, FileSystemLoader
+
+import mysql.connector
+
+
+# Creando una instancia de la App SANIC
+app = Sanic(__name__)
+
+# Configurando la conexión a la DB
+db = mysql.connector.connect(
+	host = "localhost",
+	user = "root",
+	password = "D354rr0ll0f4t1g4",
+	database = "dbfatiga"
+	)
+
+# Definir las rutas del CRUD
+## Ruta para obtener todos los registros;
+"""
+@app.route("/usuarios", methods=["GET"])
+async def obtener_usuarios(request):
+    cursor = db.cursor()
+    cursor.execute("SELECT nombre,usuario FROM tbUsuario")
+    usuarios = cursor.fetchall()
+    #print([usuarios for u in usuarios])
+
+    return json(usuarios)
+
+"""
+@app.route("/usuarios", methods=["GET"])
+async def obtener_usuarios(request):
+    cursor = db.cursor()
+    cursor.execute("SELECT idUsuario,nombre,usuario FROM tbUsuario")
+    usuarios = cursor.fetchall()
+
+    env = Environment(loader=FileSystemLoader("templates"))
+    template = env.get_template("usuarios.html")
+    rendered_template = template.render(usuarios=usuarios)
+
+    return response.html(rendered_template)
+
+# Ruta para actualizar un registro existente
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8900)
